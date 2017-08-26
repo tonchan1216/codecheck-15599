3858f1cb37a8ec6980f52bee3e132e466a53e6d0
tonchan1216 <viva.la.vida27315@gmail.com>
2017-08-26 18:24:22 +0900

    /抜き

--- app/Main.php
+++ app/Main.php
@@ -9,7 +9,7 @@ function run ($argc, $argv)
 
 		$curl = curl_init();
 
-		curl_setopt($curl, CURLOPT_URL, $base_url.'/api/hash?q='.$q);
+		curl_setopt($curl, CURLOPT_URL, $base_url.'api/hash?q='.$q);
 		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 証明書の検証を行わない
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);  // curl_execの結果を文字列で返す
